9388ecddef53b6e806a33cbe22600509f6788471
Tim Steeman <tmdev82@gmail.com>
2024-07-21 19:48:36 +0200

    implemented CORS configuration to allow front end to send requests to back end

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 sample_data = {
     "title": "This is a test note.",
