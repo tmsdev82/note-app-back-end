a2f61ed0d52f4a7ecc744992ff91a58ba16fb8ff
Tim Steeman <tmdev82@gmail.com>
2024-08-02 15:23:36 +0200

    implemented POST notes endpoint

--- main.py
+++ main.py
@@ -40,3 +40,16 @@ async def get_notes():
     with open(DATA_FILENAME, "r") as note_data_file:
         note_data = json.load(note_data_file)
         return note_data
+
+
+@app.post("/notes")
+async def add_notes(new_note: dict):
+    current_note_data = []
+    if os.path.isfile(DATA_FILENAME):
+        with open(DATA_FILENAME, "r") as note_data_file:
+            current_note_data = json.load(note_data_file)
+
+    current_note_data.append(new_note)
+
+    with open(DATA_FILENAME, "w") as write_file:
+        json.dump(current_note_data, write_file)
