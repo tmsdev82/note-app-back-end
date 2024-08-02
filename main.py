6c122862b8bd77867b96ddb5d0c735b39da5f970
Tim Steeman <tmdev82@gmail.com>
2024-08-02 21:46:53 +0200

    implemented update note endpoint

--- main.py
+++ main.py
@@ -67,3 +67,12 @@ async def delete_note(index: int):
 
     with open(DATA_FILENAME, "w") as write_file:
         json.dump(current_note_data, write_file)
+
+
+@app.put("/notes/{index}")
+async def update_note(index: int, updated_note: Note):
+    current_note_data = load_notes_data()
+    current_note_data[index] = updated_note.model_dump()
+
+    with open(DATA_FILENAME, "w") as write_file:
+        json.dump(current_note_data, write_file)
