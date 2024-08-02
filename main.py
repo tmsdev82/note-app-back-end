148a1b80f3763970af406bf89223811d3c8270d5
Tim Steeman <tmdev82@gmail.com>
2024-08-02 14:48:15 +0200

    implemented GET notes endpoint

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+import json
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+DATA_FILENAME = "notes-data.json"
 
 app = FastAPI()
 
@@ -24,3 +29,14 @@ sample_data = {
 @app.get("/")
 async def read_root():
     return sample_data
+
+
+@app.get("/notes")
+async def get_notes():
+    if not os.path.isfile(DATA_FILENAME):
+        print("No data found!")
+        return []
+
+    with open(DATA_FILENAME, "r") as note_data_file:
+        note_data = json.load(note_data_file)
+        return note_data
